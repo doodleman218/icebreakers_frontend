5a7f6b5a2d766318c6c138a0d631df6f38b5a5a5
doodleman218 <dcwolff218@gmail.com>
2020-06-24 09:20:31 -0500

    routes work

---
+++ src/containers/createRoom.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class createRoom extends Component {
+  render() {
+    return (
+      <div>
+        create a room
+      </div>
+    )
+  }
+}
+
+export default createRoom
---
+++ src/containers/room.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class room extends Component {
+  render() {
+    return (
+      <div>
+        this is a room
+      </div>
+    )
+  }
+}
+
+export default room
