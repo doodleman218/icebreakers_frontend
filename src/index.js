557026f0868e7910b07320e120d7882265d34c35
doodleman218 <dcwolff218@gmail.com>
2020-07-03 13:06:32 -0500

    install new actioncable and working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ActionCableProvider } from 'react-actioncable-provider';
+import { ActionCableProvider } from '@thrash-industries/react-actioncable-provider';
+
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
