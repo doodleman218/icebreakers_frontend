4b011617a8d61a18bd5dedaf0cf2b0c90e4c9ddc
doodleman218 <dcwolff218@gmail.com>
2020-07-07 18:41:49 -0500

    some css, working on bug

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,20 +1,30 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import login from "./login";
 
 export class home extends Component {
   render() {
     return (
       <div>
-        WELCOME TO ICEBREAKERS
-        <div className="home-buttons">
-        <a type="button" className="btn btn-info" href="create_room/" role="button">Create a Game</a>
-        <br></br>
-        <a href="login/">Join a Game</a>
+        <h1>WELCOME TO ICEBREAKERS</h1>
+        <div className="homeBtnContainer" >
+          <Link to="/create_room">
+            <button className="homeCreateBtn">Create a Room</button>
+          </Link>
+          </div>
+          <div>
+          <Link to="/login">
+            <button className="homeJoinBtn">Join a Room</button>
+          </Link>
+          {/* <a href="login/">Join a Game</a> */}
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default home
+export default home;
+
+{
+  /* <a type="button" className="btn btn-info" href="create_room/" role="button">Create a Game</a> */
+}
