9dea73bfc1446c84e1b0643dcd02339dfd735f68
doodleman218 <dcwolff218@gmail.com>
2020-08-18 14:34:07 -0500

    refactor gameText clean up old code

--- src/components/gameText.js
+++ src/components/gameText.js
@@ -1,59 +1,39 @@
 import React, { Component } from "react";
 
 export class gameText extends Component {
-
-renderText = () => {
-  let player = this.props.currentPlayer;
+  renderGameText = () => {
+    let player = this.props.currentPlayer;
     let question = this.props.currentQuestion.content;
-return (
-    <div>
-    <h3 className="currentPlayer">{player}</h3>
-    <h3 className="currentQuestion">{question}</h3>
-    <br></br>
-    {this.props.playerButton()}
-    {this.props.hostButton()}
-  </div>
-)
-}
+    return (
+      <div>
+        <h3 className="currentPlayer">{player}</h3>
+        <h3 className="currentQuestion">{question}</h3>
+        <br></br>
+        {this.props.playerButton()}
+        {this.props.hostButton()}
+      </div>
+    );
+  };
 
-callReset = (resetFunc) => {
-  setTimeout(resetFunc, 2000);
-}
+  callReset = (resetFunc) => {
+    setTimeout(resetFunc, 2000);
+  };
 
   gameText = () => {
-    // let player = this.props.currentPlayer;
-    // let question = this.props.currentQuestion.content;
-
-    if (this.props.reshufflingQuestions && this.props.reshufflingUsers === true) {
-      this.callReset(this.props.resetUsersAndQuestionsShuffle)
-      // setTimeout(() => {
-      //   this.props.resetUsersAndQuestionsShuffle()
-      // }, 2000);
-        return <h3>Reshuffling Questions and Users...</h3>
+    if (
+      this.props.reshufflingQuestions &&
+      this.props.reshufflingUsers === true
+    ) {
+      this.callReset(this.props.resetUsersAndQuestionsShuffle);
+      return <h3>Reshuffling Questions and Users...</h3>;
     } else if (this.props.reshufflingUsers === true) {
-      this.callReset(this.props.resetUsersShuffle)
-      // setTimeout(() => {
-      //   this.props.resetUsersShuffle()
-      // }, 2000);
-        return <h3>Reshuffling Users...</h3>
+      this.callReset(this.props.resetUsersShuffle);
+      return <h3>Reshuffling Users...</h3>;
     } else if (this.props.reshufflingQuestions === true) {
-      this.callReset(this.props.resetQuestionsShuffle)
-      // setTimeout(() => {
-      //   this.props.resetQuestionsShuffle()
-      // }, 2000);
-        return <h3>Reshuffling Questions...</h3>
+      this.callReset(this.props.resetQuestionsShuffle);
+      return <h3>Reshuffling Questions...</h3>;
     } else {
-      
-      return this.renderText()
-      // return (
-      //   <div>
-      //     <h3 className="currentPlayer">{player}</h3>
-      //     <h3 className="currentQuestion">{question}</h3>
-      //     <br></br>
-      //     {this.props.playerButton()}
-      //     {this.props.hostButton()}
-      //   </div>
-      // );
+      return this.renderGameText();
     }
   };
 
@@ -63,4 +43,3 @@ callReset = (resetFunc) => {
 }
 
 export default gameText;
-
