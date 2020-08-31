8d1957d33338031097767fb83a2bdf9ddcea5f22
doodleman218 <dcwolff218@gmail.com>
2020-08-30 19:40:32 -0500

    bootstrap navbar start work on logo and columns

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,31 +1,56 @@
-import React, { Component } from 'react'
-import icebreakersv8 from "../logo/icebreakersv8.png"
+import React, { Component } from "react";
+import icebreakersv8 from "../logo/icebreakersv8.png";
+import { Container, Row, Col } from "react-bootstrap";
+
 
 export class navBar extends Component {
-  
   logOutBtn = () => {
     if (this.props.currentUser === this.props.host) {
-      return <button className="LogoutBtn" onClick={this.props.endGameBtn}>End Game</button>
+      return (
+        <button className="LogoutBtn" onClick={this.props.endGameBtn}>
+          End Game
+        </button>
+      );
     } else {
-      return <button className="LogoutBtn" onClick={this.props.logoutBtn}>Logout</button>
+      return (
+        <button className="LogoutBtn" onClick={this.props.logoutBtn}>
+          Logout
+        </button>
+      );
     }
-  }
-  
+  };
+
   render() {
     return (
       <div className="NavBar">
+        <Col className="col-4">
         {this.logOutBtn()}
-        <div className="NavBarTitle"><img className="navLogo"src={icebreakersv8} alt="icebreakers logo"/></div>
-        {/* <div className="NavBarInfoParent"> */}
-          <span className="NavBarInfo"> 
-            <div className="navBarInfoRoom">Room</div>
-            <div className="navBarInfoName">{this.props.room}  </div>
-          </span>
-          
-        </div>
-      // </div>
-    )
+        </Col>
+        <Col className="col-4 align-self-center">
+        {/* <div className="NavBarTitle"> */}
+          <img className="navLogo" src={icebreakersv8} alt="icebreakers logo" />
+        {/* </div> */}
+        </Col>
+        <Col className="col-4 NavBarInfo">
+          <div className="navBarInfoRoom">Room</div>
+          <div className="navBarInfoName">{this.props.room} </div>
+        </Col>
+      </div>
+    );
   }
 }
 
-export default navBar
+export default navBar;
+
+
+{/* <div className="NavBar">
+{this.logOutBtn()}
+<Col className="align-self-center">
+{/* <div className="NavBarTitle"> */}
+//   <img className="navLogo" src={icebreakersv8} alt="icebreakers logo" />
+// {/* </div> */}
+// </Col>
+// <span className="NavBarInfo">
+//   <div className="navBarInfoRoom">Room</div>
+//   <div className="navBarInfoName">{this.props.room} </div>
+// </span>
